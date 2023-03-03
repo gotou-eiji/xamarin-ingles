dbac487b019d19af72f2954f84bbb30908cfa741
Eiji <99904315+gotou-eiji@users.noreply.github.com>
2023-03-03 16:57:50 -0300

    Add files via upload

---
+++ ingles_familia/PageCores.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ingles_familia
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageCores : ContentPage
+    {
+        public PageCores()
+        {
+            InitializeComponent();
+        }
+    }
+}
